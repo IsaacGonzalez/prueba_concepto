1e2fbb69dd15c05e11bc4a79f66a33a732ef5ad2
Isaac Gonzalez <isaac.gonzalez@uabc.edu.mx>
2012-11-28 22:59:26 -0800

    New Base Line
    Quitamos el spring-security-plugin
    
    y se modifico el codigo de todos los controladores para que sea
    dinamico, y se eliminaron las vistas por default, si ocupan un metodo
    extra en los controladores se agregan despues de el codigo "def
    scaffold=NombreDelControlador"

--- grails-app/domain/org/trapo/Usuario.groovy
+++ grails-app/domain/org/trapo/Usuario.groovy
@@ -1,18 +1,21 @@
 package org.trapo
 
-import org.security.User
 
-class Usuario extends User{
+
+class Usuario {
 
 	int numeroEmpleado
 	Usuario supervisor
 	String nombre
 	String apellidos	
     boolean enabled = true
+    String correo
+    String password
 	// Array de formatos ???
 
 
     static constraints = {
+        correo email: true
         password password: true
     	numeroEmpleado unique: true, nullable: false,min:1
     	nombre nullable: false, blank:false, matches: "[a-zA-ZÑñá-úÁ-Ú ]+"
