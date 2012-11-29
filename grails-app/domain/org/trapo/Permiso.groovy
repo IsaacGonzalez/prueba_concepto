de57e0b2c940d349291fb24c9c1f48dbadde30bd
Zoe Monreal <mzoe90@gmail.com>
2012-11-29 01:17:05 -0800

    Permiso1

--- grails-app/domain/org/trapo/Permiso.groovy
+++ grails-app/domain/org/trapo/Permiso.groovy
@@ -1,7 +1,17 @@
 package org.trapo
 
 class Permiso extends Solicitud{
-
-    static constraints = {
-    }
+Date fecha
+String tipoPermiso 
+Date desde
+int diasPermiso
+String motivo
+static constraints = {
+fecha blank:false, validator: { it >= new Date()-1} 
+tipopermiso blank:false,inList: ["Permiso sin goce de sueldo", 
+"Permiso económico", "Licencia con goce de sueldo", "Licencia sin goce de sueldo"]
+desde blank:false
+diaspermiso blank:false, validator: {it >= new Date()-1} 
+motivo blank:false, maxSize:2500
+}
 }
